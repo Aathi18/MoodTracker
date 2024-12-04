444f93c4298c476feb2eefb20859039eb1fb52a7
Aathi18 <152516499+Aathi18@users.noreply.github.com>
2024-12-04 05:56:52 +0530

    create title  this design App

--- lib/main.dart
+++ lib/main.dart
@@ -41,17 +41,17 @@ class MoodTrackerHomePage   extends StatefulWidget {
       _MoodTrackerHomePageState();
 
 }
-+
+
 class _MoodTrackerHomePageState extends State <MoodTrackerHomePage>{
   final List <MoodEntry> _moodLog=[];
 
   //define moods for two rows
 
-final List<Mood>_moodsRow1=[
-  Mood('Happy',Icons.sentiment_very_satisfied,Colors.yellow),
-  Mood('Sad',Icons.sentiment_dissatisfied,Colors.blue),
-  Mood('Angry',Icons.sentiment_very_dissatisfied,Colors.red),
-];
+  final List<Mood>_moodsRow1=[
+    Mood('Happy',Icons.sentiment_very_satisfied,Colors.yellow),
+    Mood('Sad',Icons.sentiment_dissatisfied,Colors.blue),
+    Mood('Angry',Icons.sentiment_very_dissatisfied,Colors.red),
+  ];
 
   final List<Mood>_moodsRow2=[
     Mood('Excited',Icons.sentiment_satisfied_alt,Colors.orange),
@@ -61,56 +61,76 @@ final List<Mood>_moodsRow1=[
 
   //Add a MoodEntry
 
-void _addMood(Mood mood,String note){
-  setState(() {
-    _moodLog.add(MoodEntry(mood: mood, date: DateTime.now(), note: note));
-  });
-}
+  void _addMood(Mood mood,String note){
+    setState(() {
+      _moodLog.add(MoodEntry(mood: mood, date: DateTime.now(), note: note));
+    });
+  }
 
 //Get the most frequent mood
-Mood _getMostFrequentMood() {
-  if (_moodLog.isEmpty) return
-    Mood('None', Icons.sentiment_neutral, Colors.grey);
+  Mood _getMostFrequentMood() {
+    if (_moodLog.isEmpty) return
+      Mood('None', Icons.sentiment_neutral, Colors.grey);
 
-  final moodCounts = <String, int>{};
-  for (var entry in _moodLog) {
-    moodCounts[entry.mood.name] = (moodCounts[entry.mood.name] ?? 0) + 1;
+    final moodCounts = <String, int>{};
+    for (var entry in _moodLog) {
+      moodCounts[entry.mood.name] = (moodCounts[entry.mood.name] ?? 0) + 1;
+    }
+    final mostFrequent = moodCounts.entries.reduce((a, b) =>
+    a.value > b.value ? a : b);
+
+    return
+      [..._moodsRow1, ..._moodsRow2].firstWhere((mood) =>
+      mood.name == mostFrequent.key);
   }
-  final mostFrequent = moodCounts.entries.reduce((a, b) =>
-  a.value > b.value ? a : b);
-
-  return
-    [..._moodsRow1, ..._moodsRow2].firstWhere((mood) =>
-    mood.name == mostFrequent.key);
-}
   //Show a dialog to long a mood
   void _showMoodDialog(Mood mood){
     final TextEditingController _noteController =TextEditingController();
 
     showDialog(
-      context: context,
-      builder: (context){
-        return AlertDialog(title: const Text("Add Mood",
-        style: TextStyle(fontWeight: FontWeight.bold)),
-        content: Column(mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("Selected Mood: ${mood.name}"),
-          const SizedBox(height: 10),
-          TextField(
-            controller: _noteController,
-            decoration: const InputDecoration(
-              labelText: "Why do you feel this way?",border: OutlineInputBorder(),
+        context: context,
+        builder: (context){
+          return AlertDialog(title: const Text("Add Mood",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+            content: Column(mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Selected Mood: ${mood.name}"),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _noteController,
+                  decoration: const InputDecoration(
+                    labelText: "Why do you feel this way?",border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
             ),
+          actions: [TextButton(
+            onPressed: () =>Navigator.pop(context),
+            child: const Text("cancel",style: TextStyle(color: Colors.red)),
           ),
-        ],
-        ),
+          ElevatedButton(
+              onPressed: (){
+                _addMood(mood, _noteController.text);
+                Navigator.pop(context);
+              },
+              child: const Text("Add Mood"),
+            )
+           ],
+          );
+        },
+    );
+  }
 
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MoodTracker")),
+    );
 
-      }
+  }
 
-
-    
-}
 
 }
 
